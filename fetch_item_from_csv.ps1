cfe63f30910227a1d904c08b261a0e6d3d5828a3
nick-stuckless <156607156+nick-stuckless@users.noreply.github.com>
2025-02-17 15:08:53 -0500

    Fetch item from csv
    - Powershell script that will fetch a specific item from every line in a csv file

---
+++ fetch_item_from_csv.ps1
@@ -0,0 +1,17 @@
+# Define the path to CSV file
+$csvPath = "C:\Users\nicks\Downloads\packets.csv"
+
+# Read the CSV file as plain text
+$lines = Get-Content $csvPath
+
+# Iterate through each line and extract the last item
+foreach ($line in $lines) {
+    # Split the line by commas
+    $fields = $line -split ","
+    
+    # Get the item
+    $Item = $fields[-2]
+
+    # Output the last item
+    Write-Output $Item
+}
